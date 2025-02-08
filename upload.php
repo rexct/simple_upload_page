cc54fe1359e003791b7713a45afc30e4a17c53d2
Dvaid Chen <david-chen@aifa.com.tw>
2025-02-08 09:25:06 +0800

    Add delete file link.

--- upload.php
+++ upload.php
@@ -16,6 +16,34 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     exit; // Terminate script execution after file upload is processed.
 }
 
+$delete_info = '';
+
+if (isset($_GET['id']) && isset($_GET['size'])) {
+    $file_index = (int) $_GET['id'];
+    $expected_size = (int) $_GET['size'];
+
+    // Get list of files, excluding '.' and '..'
+    $files = array_values(array_diff(scandir($destination_path), array('.', '..')));
+
+    // Check if the index is within the range of available files
+    if ($file_index >= 0 && $file_index < count($files)) {
+        $target_file = $destination_path . $files[$file_index];
+
+        // Check if the file exists and matches the expected size
+        if (file_exists($target_file) && filesize($target_file) === $expected_size) {
+            if (unlink($target_file)) {
+                $delete_info = "File '{$files[$file_index]}' successfully deleted.";
+            } else {
+                $delete_info = "Error: Unable to delete the file.";
+            }
+        } else {
+            $delete_info = "Error: File size does not match the expected size or file does not exist.";
+        }
+    } else {
+        $delete_info = "Error: File index is out of range.";
+    }
+}
+
 $files = array_diff(scandir($destination_path), array('.', '..'));
 ?>
 
@@ -27,15 +55,19 @@ $files = array_diff(scandir($destination_path), array('.', '..'));
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 <body>
+    <i><?= $delete_info ?></i>
     <div id="drop_zone" onclick="document.getElementById('fileInput').click();">Drop files here or click to select</div>
     <input type="file" id="fileInput" style="display: none;" multiple>
 
     <div id="file_list">
         <h3>Uploaded files:</h3>
         <ul>
-            <?php foreach($files as $file): ?>
-                <li><a href="<?php echo $destination_path . $file; ?>" download><?php echo $file; ?></a></li>
-            <?php endforeach; ?>
+            <?php $id = 0; foreach($files as $file): ?>
+                <li>
+                    <a href="<?php echo $destination_path . $file; ?>" download><?php echo $file; ?></a> &nbsp;
+                    <a href="./upload.php?id=<?= $id ?>&size=<?=filesize($destination_path . $file)?>"  >&#x2613</a>
+                </li>
+            <?php $id++; endforeach; ?>
         </ul>
     </div>
 
